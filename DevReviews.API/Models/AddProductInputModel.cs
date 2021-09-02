81e4fe5bd97935a1c78ce07ec148eee09c132689
Gustavo Pereira <gustavopereirasantos@hotmail.com>
2021-09-02 17:20:40 -0300

    Deletando o projeto para começar do zero

--- DevReviews.API/Models/AddProductInputModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DevReviews.API.Models
-{
-    public class AddProductInputModel
-    {
-        public string Title { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-    }
-}
--- DevReviews.API/Models/UpdateProductInputModel.cs
+++
@@ -1,8 +0,0 @@
-namespace DevReviews.API.Models
-{
-    public class UpdateProductInputModel
-    {
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-    }
-}
